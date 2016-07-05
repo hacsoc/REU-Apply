8240b3f185cfeb5c32887bd6b5386ee6d449c64b
smithy545 <smithy545@gmail.com>
2016-07-05 14:35:09 -0400

    moving to express

--- imports/startup/server/index.js
+++
@@ -1,6 +0,0 @@
-import { REUs } from '../../api/REUs.js';
-import { Meteor } from 'meteor/meteor';
-
-Meteor.publish('reus', function reuPub() {
-	return REUs.find({});
-});
--- imports/ui/pages/pages.js
+++
@@ -1,6 +0,0 @@
-import "./about.html";
-import "./index.html";
-
-import "./reu-list.js";
-import "./sign-up.js";
-import "./sign-in.js";
