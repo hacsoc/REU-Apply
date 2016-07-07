0332d4466218f8c9ca11dff83971b240373e5464
EC2 Default User <ec2-user@ip-172-31-62-21.ec2.internal>
2016-07-07 21:15:05 +0000

    worked on linked in auth

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var passport = require('passport');
 var validator = require('validator');
+var request = require('request');
 
 var User = require('../models/user');
 
@@ -10,6 +11,11 @@ router.get('/sign-up', function(req, res) {
     res.render('sign-up', { flash: req.flash('signupMessage') });
 });
 
+router.get('/login', function(req, res) {
+    res.render('login', { flash: req.flash('loginMessage') });
+});
+
+// process the sinup form
 router.post('/sign-up', passport.authenticate('local-signup', {
     successRedirect : '/profile/setup', // redirect to the secure profile section
     failureRedirect : '/sign-up', // redirect back to the signup page if there is an error
@@ -64,15 +70,63 @@ router.get('/', function(req, res) {
     res.redirect('/home');
 });
 
+router.get('/auth/linkedin/callback', function(req, res) {
+    if(req.query.error) throw req.query.error;
+    if(req.query.state != "0928354") throw "CSRF Attack error";
+
+    if(req.user.linkedin.token) {
+	var options = {
+	    url: "https://api.linkedin.com/v1/people/~?format=json",
+	    headers: {
+		Authorization: "Bearer " + req.user.linkedin.token,
+	    }
+	};
+	request.post(options, function(err, resp, body) {
+	    if(err) throw err;
+	    if(body.error) throw body.error;
+	    res.render('profile', { user: req.user });
+	});
+    } else {
+	var options = {
+	    uri: 'https://www.linkedin.com/oauth/v2/accessToken',
+	    headers: {
+	    },
+	    qs: {
+		client_id: '77tgqz6flgej2j',
+		client_secret: 'jGjoCdApJjgkGtdG',
+		grant_type: 'authorization_code',
+		redirect_uri: "http://reu-apply.com/auth/linkedin/callback",
+		code: req.query.code
+	    },
+            headers: {
+		'Content-Type': 'application/x-www-form-urlencoded',
+            }
+	};
+    
+	request.post(options, function(err, resp, body) {
+	if(err) throw err;
+	    if(body.error) throw body.error;
+	    var options = {
+		url: "https://api.linkedin.com/v1/people/~?format=json",
+		headers: {
+		    Authorization: "Bearer " + body.access_token,
+		}
+	    };
+	    request.post(options, function(err, resp, body) {
+		if(err) throw err;
+		if(body.error) throw body.error;
+		res.render('profile', { user: req.user });
+	    });
+	});
+    }
+});
 
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) {
         return next();
     }
-    var ctx = {
-        flash: req.flash('loginMessage'),
-    };
-    res.render('login', ctx);
+
+    res.redirect('/login');
 }
 
 module.exports = router;
